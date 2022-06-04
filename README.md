43949d6dd4a057d1ca37994c4908b3b2d0160240
raimoncoding <robertjanstefan@gmail.com>
2022-06-04 10:43:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-4c87zz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-4c87zz)
---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./Detail";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h1 className="name">{props.id}</h1>
+        <h2 className="name">{props.name}</h2>
+        <Avatar img={props.img} />
+      </div>
+      <div className="bottom">
+        <Detail detailInfo={props.tel} />
+        <Detail detailInfo={props.email} />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
